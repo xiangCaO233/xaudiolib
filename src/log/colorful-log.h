d9a84835c371b2f7b3c732d012cbac81b15741a2
2333xiang <odokoei@outlook.com>
2025-01-30 02:03:20 +0800

    日志实现

--- src/log/colorful-log.h
+++ src/log/colorful-log.h
@@ -1,4 +1,18 @@
 ﻿#ifndef XIANG_COLORFULLOG_H
 #define XIANG_COLORFULLOG_H
 
+#include <spdlog/sinks/base_sink.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
+#include <memory>
+
+class XLogger {
+  // 日志实体
+  static std::shared_ptr<spdlog::logger> logger;
+
+ public:
+  static void init();
+};
 #endif  // XIANG_COLORFULLOG_H
--- src/log/colorful_log.cpp
+++ src/log/colorful_log.cpp
@@ -1,1 +1,18 @@
-﻿#include <glog/logging.h>
+﻿#include <memory>
+
+#include "colorful-log.h"
+
+std::shared_ptr<spdlog::logger> XLogger::logger;
+
+void XLogger::init() {
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+      "logs/logs.log", true);
+
+  std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
+  logger = std::make_shared<spdlog::logger>("multi_sink", sinks.begin(),
+                                            sinks.end());
+  spdlog::register_logger(logger);
+  spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
+  logger->info("日志初始化完成");
+}
