8dc44e0e75c2e35cf2dd1442c3c43a096142fa04
xiangCaO233 <odokoei@outlook.com>
2025-05-24 20:42:15 +0800

    添加流关闭操作

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -21,46 +21,48 @@ sampleratetype Config::samplerate = sampleratetype::AVMEDIA_SAMPLERATE;
 int Config::play_buffer_size = 256;
 // 保存路径
 std::string Config::config_file_path = "./config/latest_config.json";
+
 void Config::to_json(json& j) {
-  j = json{{"audio_transfer_method", static_cast<int>(audio_transfer_method)},
-           {"decode_thread_count", decode_thread_count},
-           {"mix_method", static_cast<int>(mix_method)},
-           {"mix_buffer_size", mix_buffer_size},
-           {"channel", static_cast<int>(channel)},
-           {"samplerate", static_cast<int>(samplerate)},
-           {"play_buffer_size", play_buffer_size}};
+    j = json{{"audio_transfer_method", static_cast<int>(audio_transfer_method)},
+             {"decode_thread_count", decode_thread_count},
+             {"mix_method", static_cast<int>(mix_method)},
+             {"mix_buffer_size", mix_buffer_size},
+             {"channel", static_cast<int>(channel)},
+             {"samplerate", static_cast<int>(samplerate)},
+             {"play_buffer_size", play_buffer_size}};
 }
 void Config::from_json(const json& j) {
-  audio_transfer_method =
-      static_cast<transfertype>(j.at("audio_transfer_method").get<int>());
-  decode_thread_count = j.at("decode_thread_count").get<int>();
-  mix_method = static_cast<mixtype>(j.at("mix_method").get<int>());
-  mix_buffer_size = j.at("mix_buffer_size").get<int>();
-  channel = static_cast<channels>(j.at("channel").get<int>());
-  samplerate = static_cast<sampleratetype>(j.at("samplerate").get<int>());
-  play_buffer_size = j.at("play_buffer_size").get<int>();
+    audio_transfer_method =
+        static_cast<transfertype>(j.at("audio_transfer_method").get<int>());
+    decode_thread_count = j.at("decode_thread_count").get<int>();
+    mix_method = static_cast<mixtype>(j.at("mix_method").get<int>());
+    mix_buffer_size = j.at("mix_buffer_size").get<int>();
+    channel = static_cast<channels>(j.at("channel").get<int>());
+    samplerate = static_cast<sampleratetype>(j.at("samplerate").get<int>());
+    play_buffer_size = j.at("play_buffer_size").get<int>();
 }
 
 void Config::load() {
-  std::ifstream input(config_file_path);
-  if (input) {
-    json j;
-    input >> j;
-    from_json(j);
-  } else {
-    XERROR("无法打开配置文件: " + config_file_path);
-  }
+    std::ifstream input(config_file_path);
+    if (input) {
+        json j;
+        input >> j;
+        from_json(j);
+    } else {
+        XERROR("无法打开配置文件: " + config_file_path);
+    }
 };
 
 void Config::save() {
-  json j;
-  to_json(j);
-  std::ofstream output(config_file_path);
-  if (output) {
-    // 美化输出，缩进为4
-    output << j.dump(4);
-  } else {
-    XERROR("无法保存配置文件!");
-  }
+    json j;
+    to_json(j);
+    std::ofstream output(config_file_path);
+    if (output) {
+        // 美化输出，缩进为4
+        output << j.dump(4);
+    } else {
+        XERROR("无法保存配置文件!");
+    }
+    output.close();
 }
 }  // namespace x
